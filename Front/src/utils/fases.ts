2b23dd565482777d748c1efa43852a65e8445b0a
Walter <106196739+waltercamino@users.noreply.github.com>
2026-09-22 18:10:21 -0300

    eliminar código legacy obsoleto (mocks, mappers, utils y componentes huérfanos)

--- Front/src/utils/fases.ts
+++
@@ -1,8 +0,0 @@
-import { fases as getFases, type FaseEvento } from '@/config/eventoConfig'
-import { estaEnVentana } from './ventanas'
-
-export const getFaseActual = (hora: number): FaseEvento | null => {
-  const activas = getFases().filter(f => estaEnVentana(hora, f.rango))
-  if (!activas.length) return null
-  return activas[0]
-}
